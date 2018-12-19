8575cf895e4174fbd9abd988dc6ca45d1859e91b
dxd_sjtu <dxd_sjtu@outlook.com>
2018-12-19 09:30:26 +0800

    feat: enable cli

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
+#! /usr/bin/env node
 const puppeteer = require('puppeteer');
 const testPage = require('./testPage');
-const logger=require('./logger');
+const logger = require('./logger');
+const program = require('commander');
 
-(async () => {
-  let url="https://google.com";
-  let browser = await puppeteer.launch();
-  let page = await browser.newPage();
-  logger(await testPage(page,url),"json");
- // console.log(await testPage(page)); // first enter
- // console.log(await testPage(page)); // second enter with cache and sw
-  await browser.close();
+program
+  .arguments('<url>')
+  .action(async (url) => {
+    let browser = await puppeteer.launch();
+    let page = await browser.newPage();
+    logger(await testPage(page, url), "json");
+    // console.log(await testPage(page)); // first enter
+    // console.log(await testPage(page)); // second enter with cache and sw
+    await browser.close();
 
-  // browser = await puppeteer.launch();
-  // page = await browser.newPage();
-  // await testPage(page); // only for creating fresh instance
-  // await page._client.send('ServiceWorker.enable');
-  // await page._client.send('ServiceWorker.stopAllWorkers');
-  // console.log(await testPage(page)); // second enter only with cache
-  // await browser.close();
+    // browser = await puppeteer.launch();
+    // page = await browser.newPage();
+    // await testPage(page); // only for creating fresh instance
+    // await page._client.send('ServiceWorker.enable');
+    // await page._client.send('ServiceWorker.stopAllWorkers');
+    // console.log(await testPage(page)); // second enter only with cache
+    // await browser.close();
 
-  // browser = await puppeteer.launch();
-  // page = await browser.newPage();
-  // await testPage(page); // only for creating fresh instance
-  // await page._client.send('Network.clearBrowserCache');
-  // console.log(await testPage(page)); // second enter only with sw
-  // await browser.close();
-})();
+    // browser = await puppeteer.launch();
+    // page = await browser.newPage();
+    // await testPage(page); // only for creating fresh instance
+    // await page._client.send('Network.clearBrowserCache');
+    // console.log(await testPage(page)); // second enter only with sw
+    // await browser.close();
+  });
+program.parse(process.argv);
